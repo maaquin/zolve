e3128445d9df7fe469972a9bcbedc7289ac55d0d
lucciano <lmaquin-2019274@kinal.edu.gt>
2024-05-26 14:47:52 -0600

    se sube el backend

---
+++ back/src/store/store.model.js
@@ -0,0 +1,28 @@
+import mongoose, { Schema } from "mongoose";
+
+const storeSchema = mongoose.Schema({
+    name: {
+        type: String,
+    },
+    direction: {
+        type: String,
+    },
+    score: {
+        type: String,
+    },
+    avatarUrl: {
+        type: String,
+    },
+    imgUrl: {
+        type: String,
+    },
+    coordenadas:{
+        type: String,
+    },
+    estado:{
+        type: Boolean,
+        default: true
+    }
+})
+
+export default mongoose.model('Store', storeSchema)
